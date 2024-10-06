0c9c2a945faef74ca586e5c9ad414349f92a67b2
eren-erdgn <erener.2001@gmail.com>
2024-10-07 01:46:18 +0300

    rider setup. test script created

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("Im in start!");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log("Im in Update!");
+    }
+}
